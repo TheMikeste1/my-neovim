6d50aa29b4101b41a9b51462259f917007b46a5c
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-28 11:01:07 -0700

    Set up neotest

--- lua/plugins/ide/neotest.lua
+++ lua/plugins/ide/neotest.lua
@@ -1,11 +1,25 @@
 -- https://github.com/nvim-neotest/neotest
--- TODO: Add runners
 return {
 	"nvim-neotest/neotest",
-
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-rust"),
+				require("neotest-dotnet"),
+				require("neotest-gtest"),
+				require("neotest-bash"),
+				require("neotest-python"),
+			},
+		})
+	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
+		"rouge8/neotest-rust",
+		"Issafalcon/neotest-dotnet",
+		"alfaix/neotest-gtest",
+		"rcasia/neotest-bash",
+		"nvim-neotest/neotest-python",
 	},
 }
