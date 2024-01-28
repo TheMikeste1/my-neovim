ec65c61237d537328d25ad6393c12be611482156
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-01-28 10:29:08 -0700

    Add more telescope commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,51 @@
-local function init()
-	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-	vim.keymap.set("n", "<C-S-F>", builtin.live_grep, {})
-	vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, {})
-	vim.keymap.set("n", "<leader><leader>h", builtin.help_tags, {})
-end
-
 return {
 	"nvim-telescope/telescope.nvim",
-
 	dependencies = { "nvim-lua/plenary.nvim" },
-	init = init,
+	keys = {
+		{
+			"<C-p>",
+			"<cmd>lua require('telescope.builtin').find_files()<CR>",
+			desc = "Quick jump to files",
+		},
+		{
+			"<M-C-F>",
+			"<cmd>lua require('telescope.builtin').live_grep()<CR>",
+			desc = "Search in files",
+		},
+		{
+			"<C-f>",
+			"<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
+			desc = "Search in current buffer",
+		},
+		{
+			"<leader><leader>h",
+			"<cmd>lua require('telescope.builtin').help_tags()<CR>",
+			desc = "Search in help tags",
+		},
+		{
+			"<leader><leader>g",
+			"<cmd>lua require('telescope.builtin').git_files()<CR>",
+			desc = "Search in git files",
+		},
+		{
+			"<leader><leader>s",
+			"<cmd>lua require('telescope.builtin').spell_suggest()<CR>",
+			desc = "Search in spell suggest",
+		},
+		{
+			"<leader><leader>c",
+			"<cmd>lua require('telescope.builtin').commands()<CR>",
+			desc = "Search in commands",
+		},
+		{
+			"<leader><leader>t",
+			"<cmd>lua require('telescope.builtin').treesitter()<CR>",
+			desc = "Search in treesitter",
+		},
+		{
+			"<leader><leader>m",
+			"<cmd>lua require('telescope.builtin').marks()<CR>",
+			desc = "Search in marks",
+		},
+	},
 }
