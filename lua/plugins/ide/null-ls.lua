d7156d8aace52bc41b6a80c129930bec394b54b0
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2024-02-26 09:41:39 -0700

    Comment out deprecated null_ls builtins

--- lua/plugins/ide/null-ls.lua
+++ lua/plugins/ide/null-ls.lua
@@ -5,7 +5,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.completion.spell,
-				null_ls.builtins.diagnostics.eslint,
+				-- null_ls.builtins.diagnostics.eslint,
 
 				null_ls.builtins.formatting.stylua,
 
@@ -13,10 +13,10 @@ return {
 				null_ls.builtins.diagnostics.ansiblelint,
 
 				-- Shell/Bash
-				null_ls.builtins.diagnostics.shellcheck,
-				null_ls.builtins.code_actions.shellcheck,
+				-- null_ls.builtins.diagnostics.shellcheck,
+				-- null_ls.builtins.code_actions.shellcheck,
 				null_ls.builtins.formatting.shellharden,
-				null_ls.builtins.formatting.beautysh,
+				-- null_ls.builtins.formatting.beautysh,
 				null_ls.builtins.hover.printenv,
 			},
 		})
