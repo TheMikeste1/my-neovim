0d4a9883583b8a437cbe93fe95e1589a2e72aa29
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2024-03-20 07:54:20 -0600

    Add vim-rooter

--- init.lua
+++ init.lua
@@ -7,17 +7,17 @@ vim.loader.enable()
 require("utilities.file_utilities")
 
 -- Set working directory
-if vim.fn.argc() == 1 then
-	-- If the argument is a non-existing directory, create it and open it in the current window
-	if vim.fn.argv()[1]:sub(-1) == "/" and not IsDirectory(vim.fn.argv()[1]) then
-		-- Remove the slash
-		local path = vim.fn.argv()[1]:sub(1, -2)
-		-- Create the directory
-		vim.fn.mkdir(path, "p")
-	end
-
-	vim.cmd("cd %:h")
-end
+-- if vim.fn.argc() == 1 then
+-- 	-- If the argument is a non-existing directory, create it and open it in the current window
+-- 	if vim.fn.argv()[1]:sub(-1) == "/" and not IsDirectory(vim.fn.argv()[1]) then
+-- 		-- Remove the slash
+-- 		local path = vim.fn.argv()[1]:sub(1, -2)
+-- 		-- Create the directory
+-- 		vim.fn.mkdir(path, "p")
+-- 	end
+--
+-- 	vim.cmd("cd %:h")
+-- end
 
 require("vim_setup")
 require("plugin_setup")
