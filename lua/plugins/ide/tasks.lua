ee5246f85e245bda3e59c569e7ac4c4bf13f7345
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-03-23 15:55:05 -0600

    Update to Tasks

--- lua/plugins/ide/tasks.lua
+++ lua/plugins/ide/tasks.lua
@@ -4,12 +4,16 @@ return {
 		"nvim-lua/plenary.nvim",
 		"mfussenegger/nvim-dap",
 	},
-  opts = {
-    default_params = {
-      cmake = {
-        dap_name = "codelldb"
-      }
-    },
-    params_file = "neovim-tasks.json"
-  }
+	opts = {
+		default_params = {
+			cmake = {
+				dap_name = "codelldb",
+			},
+			cargo = {
+				dap_name = "codelldb",
+			},
+		},
+		params_file = "neovim-tasks.json",
+		dap_open_command = false,
+	},
 }
