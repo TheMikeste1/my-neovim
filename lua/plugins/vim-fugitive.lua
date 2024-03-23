7f8f20ebe98e214b5752f76a7c00aa81cbceaa65
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-03-22 20:24:21 -0600

    Fugitive updates

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,7 +1,49 @@
 return {
 	"tpope/vim-fugitive",
-  cond = true,
-  cmd = {
-    "G",
-  }
+	cond = true,
+	config = function()
+		vim.api.nvim_create_user_command("Browse", function(opts)
+			vim.fn.system({ "wslview", opts.fargs[1] })
+		end, { nargs = 1 })
+	end,
+	cmd = {
+		"G",
+		"Git",
+		"Ggrep",
+		"Git",
+		"Ggrep",
+		"Ggrep",
+		"Glgrep",
+		"Gclog",
+		"Gllog",
+		"Gcd",
+		"Glcd",
+		"Gedit",
+		"Gsplit",
+		"Gvsplit",
+		"Gtabedit",
+		"Gpedit",
+		"Gdrop",
+		"Gread",
+		"Gwrite",
+		"Gwrite",
+		"Gwq",
+		"Gwq",
+		"Gdiffsplit",
+		"Gdiffsplit",
+		"Gdiffsplit",
+		"Gvdiffsplit",
+		"Gdiffsplit",
+		"Ghdiffsplit",
+		"GMove",
+		"GRename",
+		"GDelete",
+		"GRemove",
+		"GUnlink",
+		"GBrowse",
+	},
+	dependencies = {
+		{ "tpope/vim-rhubarb" }, -- GitHub
+		{ "tommcdo/vim-fubitive" }, -- Bitbucket
+	},
 }
