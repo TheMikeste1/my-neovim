7ee796161b5bc475c51ba1372cbf66a7255f5fad
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2024-04-11 09:51:25 -0600

    Update rooter to ignore submodules

--- lua/plugins/vim-rooter.lua
+++ lua/plugins/vim-rooter.lua
@@ -1,4 +1,8 @@
 return {
 	"airblade/vim-rooter",
 	cond = true,
+	event = "VeryLazy",
+	config = function()
+		vim.g.rooter_patterns = { ".git/", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" }
+	end,
 }
