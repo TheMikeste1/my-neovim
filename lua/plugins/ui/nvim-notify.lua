1b24ce2d1548525557d7658576c8a2272fc21c0d
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2024-06-10 17:27:22 -0600

    Always notify

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -1,8 +1,10 @@
 return {
 	"rcarriga/nvim-notify",
-
+	config = function(_, opts)
+		require("notify").setup(opts)
+		vim.notify = require("notify")
+	end,
 	opts = {
-		max_width = 36,
 		minimum_width = 8,
 		render = "wrapped-compact",
 		timeout = 2000,
