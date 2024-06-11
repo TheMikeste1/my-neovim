221fead33b836508e50813ec5ab2fa05b4753613
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-06-10 20:01:16 -0600

    Remove blame codeaction

--- lua/plugins/lint/null-ls.lua
+++ lua/plugins/lint/null-ls.lua
@@ -5,7 +5,14 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.code_actions.gitsigns,
+				null_ls.builtins.code_actions.gitsigns.with({
+					config = {
+						filter_actions = function(title)
+							return title:lower():match("blame") == nil -- filter out blame actions
+						end,
+					},
+				}),
+				null_ls.builtins.code_actions.refactoring,
 				null_ls.builtins.completion.vsnip,
 
 				null_ls.builtins.completion.spell,
@@ -38,9 +45,9 @@ return {
 				null_ls.builtins.diagnostics.fish,
 				-- null_ls.builtins.formatting.fish_indent,
 
-        -- Groovy/Jenkins
-        -- null_ls.builtins.diagnostics.npm_groovy_lint,
-        -- null_ls.builtins.formatting.npm_groovy_lint,
+				-- Groovy/Jenkins
+				-- null_ls.builtins.diagnostics.npm_groovy_lint,
+				-- null_ls.builtins.formatting.npm_groovy_lint,
 
 				-- HTML/XML
 				null_ls.builtins.diagnostics.tidy,
