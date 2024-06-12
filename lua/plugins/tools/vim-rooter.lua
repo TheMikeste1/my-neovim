eed4bdd95abeb3f5411df27cee8eb54cfea6c29f
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2024-06-12 08:52:56 -0600

    Root to *.sln and only root once

--- lua/plugins/tools/vim-rooter.lua
+++ lua/plugins/tools/vim-rooter.lua
@@ -3,6 +3,17 @@ return {
 	cond = true,
 	event = "VeryLazy",
 	config = function()
-		vim.g.rooter_patterns = { ".git/", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" }
+		vim.g.rooter_patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "*.sln" }
+		local id = 0
+		local function delete_cmd()
+			vim.api.nvim_del_autocmd(id)
+		end
+		id = vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "RooterChDir",
+			callback = function()
+				vim.cmd("RooterToggle")
+				delete_cmd()
+			end,
+		})
 	end,
 }
