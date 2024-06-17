9d380ad7bdb516c7de5bad2d9a935f61ddb7f9ed
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-06-17 16:13:16 -0600

    Fix up luals

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,23 +1,32 @@
 local function on_lua_init(client)
 	local path = client.workspace_folders[1].name
-	if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-		client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using
-					-- (most likely LuaJIT in the case of Neovim)
-					version = "LuaJIT",
-				},
-				-- Make the server aware of Neovim runtime files
-				workspace = {
-					checkThirdParty = true,
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-			},
-		})
-
-		client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+	if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+		return true
 	end
+
+	client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua or {}, {
+		runtime = {
+			version = "LuaJIT",
+		},
+		diagnostics = {
+			globals = {
+				"vim",
+				"require",
+			},
+		},
+		workspace = {
+			checkThirdParty = true,
+			library = vim.api.nvim_get_runtime_file("", true),
+		},
+		doc = {
+			privateName = { "^_" },
+		},
+		telemetry = {
+			enable = false,
+		},
+	})
+
+	client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
 	return true
 end
 
@@ -49,7 +58,7 @@ return {
 				["bashls"] = function()
 					lspconfig.bashls.setup({
 						capabilities = capabilities,
-						filetypes = { "bash", "sh",  "zsh" },
+						filetypes = { "bash", "sh", "zsh" },
 					})
 				end,
 				["clangd"] = function()
--- lua/plugins/vim-dev/lazydev.lua
+++ lua/plugins/vim-dev/lazydev.lua
@@ -5,6 +5,7 @@ return {
 	},
 	opts = {
 		library = {
+			"neoconf",
 			"neotest",
 			"nvim-dap-ui",
 		},
