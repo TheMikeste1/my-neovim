3a780ca7b56c910cb33ef605a9c7e45c60378beb
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-09-29 20:11:01 -0600

    Add Trouble keymaps

--- lua/plugins/ui/trouble.lua
+++ lua/plugins/ui/trouble.lua
@@ -13,4 +13,16 @@ return {
 			},
 		},
 	},
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+	},
 }
