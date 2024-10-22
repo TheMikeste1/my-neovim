8d4db575b8536227efa1d3c3c784ecf3cd1ac887
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2024-10-22 17:00:01 -0600

    Change lightbulb

--- lua/plugins/ui/nvim-lightbulb.lua
+++ lua/plugins/ui/nvim-lightbulb.lua
@@ -1,4 +1,13 @@
 return {
 	"kosayoda/nvim-lightbulb",
-	opts = { autocmd = { enabled = true } },
+	opts = {
+		autocmd = { enabled = true },
+		code_lenses = false,
+		sign = {
+			enabled = false,
+		},
+		virtual_text = {
+			enabled = true,
+		},
+	},
 }
