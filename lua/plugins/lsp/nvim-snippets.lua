e8b1da3266560e2330eabb5127afcf74adad1a51
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-10-24 21:25:48 -0600

    Workaround for zig snippets

--- lua/plugins/lsp/nvim-snippets.lua
+++ lua/plugins/lsp/nvim-snippets.lua
@@ -5,5 +5,14 @@ return {
 		friendly_snippets = true,
 		create_cmp_source = true,
 	},
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = {
+		{
+			-- I'll have to use this until the zig snippets are updated
+      -- https://github.com/rafamadriz/friendly-snippets/pull/504
+      -- https://github.com/rafamadriz/friendly-snippets/pull/509
+			"gmassman/friendly-snippets",
+			branch = "patch-1",
+		},
+		-- "rafamadriz/friendly-snippets"
+	},
 }
