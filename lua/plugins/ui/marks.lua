a4c0c7d221ad25a9b46b862b24f60da898fad148
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-11-10 14:48:35 -0700

    More FT exclusions

--- lua/plugins/ui/marks.lua
+++ lua/plugins/ui/marks.lua
@@ -4,7 +4,20 @@ return {
   config = true,
   opts = {
     builtin_marks = { ".", "<", ">", "^", "`", "[", "]" },
-    excluded_filetypes = { "DressingInput", "noice", "Telescope", "Neotest Summary", "" },
+    excluded_filetypes = {
+      "",
+      "DressingInput",
+      "noice",
+      "Telescope",
+      "Neotest Summary",
+      "dapui_console",
+      "dapui_repl",
+      "dapui_stacks",
+      "dapui_scopes",
+      "dapui_watches",
+      "dapui_breakpoints",
+      "dap-repl",
+    },
     excluded_buftypes = { "nofile" },
   },
 }
