4b7a7e08d173ad555ee86fb41429228ce7008304
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2024-12-01 19:13:04 -0700

    Switch back to original friendly snippets

--- lua/plugins/lsp/nvim-snippets.lua
+++ lua/plugins/lsp/nvim-snippets.lua
@@ -6,13 +6,6 @@ return {
     create_cmp_source = true,
   },
   dependencies = {
-    {
-      -- I'll have to use this until the zig snippets are updated
-      -- https://github.com/rafamadriz/friendly-snippets/pull/504
-      -- https://github.com/rafamadriz/friendly-snippets/pull/509
-      "gmassman/friendly-snippets",
-      branch = "patch-1",
-    },
-    -- "rafamadriz/friendly-snippets"
+    "rafamadriz/friendly-snippets"
   },
 }
