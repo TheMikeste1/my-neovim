7119ca61316ca9328beab1f158527adec3aa069f
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2024-12-15 18:53:05 -0700

    Add GDScript

--- lua/plugins/tools/neogen.lua
+++ lua/plugins/tools/neogen.lua
@@ -1,8 +1,18 @@
 return {
   "danymat/neogen",
   cond = true,
-  config = true,
-  opts = {
-    snippet_engine = "nvim",
+  config = function()
+    require("neogen").setup({
+      snippet_engine = "nvim",
+      languages = {
+        ["gdscript"] = require("neogen.configurations.gdscript"),
+      },
+    })
+  end,
+  dependencies = {
+    {
+      "TheMikeste1/neogen-gdscript",
+      cond = true,
+    },
   },
 }
