7c8d4cb5e4fcf2b063733c28f67d96d2fefcae3f
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2025-03-12 09:32:40 -0600

    Simple commands for CMakeSeer and Snacks

--- lua/plugins/local.lua
+++ lua/plugins/local.lua
@@ -1,12 +1,33 @@
 local vscode_kit_file = vim.fn.expand("~/.local/share/CMakeTools/cmake-tools-kits.json")
 
+local function handle_api_command(opts)
+  vim.notify("CMakeSeer opts: " .. vim.inspect(opts), vim.log.levels.DEBUG)
+  if opts.fargs and opts.fargs[1] == "select_kit" then
+    require("cmakeseer").select_kit()
+    return
+  end
+
+  vim.notify("Unknown CMakeSeer command: " .. opts.args)
+end
+
 return {
   "TheMikeste1/cmakeseer.nvim",
   dir = "~/projects/cmakeseer",
+  config = function(_, opts)
+    require("cmakeseer").setup(opts)
+    vim.api.nvim_create_user_command(
+      "CMakeSeer",
+      handle_api_command,
+      { desc = "Access the CMakeSeer API", nargs = "*" }
+    )
+  end,
   opts = {
     kit_paths = {
       vscode_kit_file,
     },
     persist_file = vscode_kit_file,
+  },
+  cmd = {
+    "CMakeSeer",
   },
 }
--- lua/plugins/tools/snacks.lua
+++ lua/plugins/tools/snacks.lua
@@ -1,7 +1,21 @@
+local function handle_api_command(opts)
+  vim.notify("Snacks opts: " .. vim.inspect(opts), vim.log.levels.DEBUG)
+  if opts.fargs and opts.fargs[1] == "resume" then
+    require("snacks").picker.resume()
+    return
+  end
+
+  vim.notify("Unknown Snacks command: " .. opts.args)
+end
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  config = function(_, opts)
+    require("snacks").setup(opts)
+    vim.api.nvim_create_user_command("Snacks", handle_api_command, { desc = "Access the Snacks API", nargs = "*" })
+  end,
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
