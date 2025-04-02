a416f0969bd1d30953d83ba48cca2126a7bc3b39
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2025-04-01 20:30:03 -0600

    Refresh spell files

--- init.lua
+++ init.lua
@@ -25,4 +25,20 @@ require("keymap")
 require("filetype")
 require("configure_theme")
 
--- TODO: Compile spell files: https://www.reddit.com/r/vim/comments/5zhpre/comment/df0xccw/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+-- Recompile spell file if it updated
+local spell_path = vim.fn.stdpath("config") .. "/spell"
+local paths = vim.split(vim.fn.glob(spell_path .. "/*.add"), "\n")
+for _, file in ipairs(paths) do
+  -- Can we skip generation?
+  local compiled_file = file .. ".spl"
+  if vim.fn.filereadable(compiled_file) == 1 then
+    local file_timestamp = vim.fn.getftime(file)
+    local compiled_timestamp = vim.fn.getftime(compiled_file)
+    if file_timestamp <= compiled_timestamp then
+      goto continue
+    end
+  end
+
+  vim.cmd("mkspell! " .. file)
+  ::continue::
+end
