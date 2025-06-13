7c193810e9d483c50fc60eb77c41689aac663dff
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2025-06-13 15:56:04 -0600

    Account for VS Code

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,6 +1,10 @@
 -- C++ will also execute this file
 vim.fn.mkdir(".cache/cppcheck", "p")
 
+if VSCODE then
+  return
+end
+
 vim.keymap.set("n", "<M-o>", function()
   require("clangd_extensions.switch_source_header").switch_source_header()
 end, { noremap = true, desc = "Switch between source and header" })
--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,3 +1,7 @@
+if VSCODE then
+  return
+end
+
 local dap = require("dap")
 local dap_config = require("configs.dap.c")
 dap.adapters.cpp = dap_config.adapters()
