3dbb0b4186f0c4e319863f97cda20187ac3cd40f
Michael Hegerhorst <michael.hegerhorst+github@gmail.com>
2025-06-24 20:41:53 -0600

    Auto-enable Mason LSPs

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,65 +1,82 @@
+local ENSURE_INSTALLED = {
+  "editorconfig-checker",
+  -- Ansible
+  "ansible-lint",
+  -- C++
+  "clangd",
+  "clang-format",
+  "codelldb",
+  -- CMake
+  "cmakelang",
+  "cmakelint",
+  "neocmakelsp",
+  -- CSS
+  "stylelint",
+  -- Docker
+  "hadolint",
+  -- Lua
+  "lua-language-server",
+  "stylua",
+  -- Make
+  "checkmake",
+  -- Python
+  "black",
+  "isort",
+  "pyright",
+  "debugpy",
+  "mypy",
+  "pylint",
+  -- Shell/Bash
+  "shellharden",
+  "beautysh",
+  "shellcheck",
+  "bash-language-server",
+  -- SQL
+  "sqlfluff",
+  -- Web
+  "biome",
+  "prettier",
+  -- YAML
+  "yamllint",
+  "yamlfix",
+  "yamlfmt",
+  -- Misc
+  "harper-ls",
+}
+
+--- LSPs that should never be enabled directly, even if installed.
+local DISABLED_LSPS = {
+  "bacon_ls",
+  "rust_analyzer",
+}
+
 return {
   "williamboman/mason.nvim",
   lazy = false,
   config = function(_, opts)
     require("mason").setup(opts)
 
-    -- Custom nvchad cmd to install all mason binaries listed https://github.com/NvChad/NvChad/blob/c78f3f0501800d02b0085ecc1f79bfc64327d01e/lua/plugins/init.lua#L137
     vim.api.nvim_create_user_command("MasonInstallAll", function()
-      vim.inspect(vim.g.mason_binaries_list)
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+      if ENSURE_INSTALLED and #ENSURE_INSTALLED > 0 then
+        vim.cmd("MasonInstall " .. table.concat(ENSURE_INSTALLED, " "))
       end
     end, {})
 
-    vim.g.mason_binaries_list = opts.ensure_installed
+    -- Auto-enable installed LSPs
+    local lsp_names = {}
+    for _, package in ipairs(require("mason-registry").get_installed_packages()) do
+      if vim.tbl_contains(package.spec.categories, "LSP") then
+        assert(package.spec.neovim.lspconfig ~= nil, "Package name was nil")
+        table.insert(lsp_names, package.spec.neovim.lspconfig)
+        goto continue
+      end
+      ::continue::
+    end
+
+    vim.lsp.enable(lsp_names, true)
+    vim.lsp.enable(DISABLED_LSPS, false)
   end,
   opts = {
-    ensure_installed = {
-      "editorconfig-checker",
-      -- Ansible
-      "ansible-lint",
-      -- C++
-      "clangd",
-      "clang-format",
-      "codelldb",
-      -- CMake
-      "cmakelang",
-      "cmakelint",
-      "neocmakelsp",
-      -- CSS
-      "stylelint",
-      -- Docker
-      "hadolint",
-      -- Lua
-      "lua-language-server",
-      "stylua",
-      -- Make
-      "checkmake",
-      -- Python
-      "black",
-      "isort",
-      "pyright",
-      "debugpy",
-      "mypy",
-      "pylint",
-      -- Shell/Bash
-      "shellharden",
-      "beautysh",
-      "shellcheck",
-      "bash-language-server",
-      -- SQL
-      "sqlfluff",
-      -- Web
-      "biome",
-      "prettier",
-      -- YAML
-      "yamllint",
-      "yamlfix",
-      "yamlfmt",
-      -- Misc
-      "harper-ls",
-    },
     ui = {
       icons = {
         package_installed = "✓",
--- lua/vim-setup/lsp.lua
+++ lua/vim-setup/lsp.lua
@@ -1,17 +1,3 @@
 vim.lsp.config("*", {
   root_markers = { ".git", ".hg" },
 })
-
-vim.lsp.enable({
-  -- "bacon_ls",
-  "bashls",
-  "clangd",
-  "gdscript",
-  "harper_ls",
-  "lemminx",
-  "lua_ls",
-  "pyrefly",
-  "pyright",
-  -- "rust_analyzer",
-  "zls",
-})
