1b7b79def4d3369413f43be64296b765d3fb922c
Michael Hegerhorst <michael.hegerhorst@sdl.usu.edu>
2025-07-21 11:30:26 -0600

    Fix spellfiles changing

--- lua/after/generate_spellfiles.lua
+++ lua/after/generate_spellfiles.lua
@@ -1,6 +1,9 @@
 -- Recompile spell file if it updated
 local paths = vim.split(vim.fn.glob(vim.fs.joinpath(vim.fn.stdpath("config"), "spell", "/*.add")), "\n")
+local spellfile = ""
 for _, file in ipairs(paths) do
+  spellfile = spellfile .. "," .. file
+
   -- Can we skip generation?
   local compiled_file = file .. ".spl"
   if vim.fn.filereadable(compiled_file) == 1 then
@@ -14,4 +17,10 @@ for _, file in ipairs(paths) do
   vim.notify(string.format("Regenerating spell file %s. . .", vim.fs.basename(file)))
   vim.cmd("mkspell! " .. file)
   ::continue::
+end
+
+if not vim.o.spellfile or #vim.o.spellfile == 0 then
+  vim.o.spellfile = spellfile:sub(2)
+else
+  vim.o.spellfile = vim.o.spellfile .. spellfile
 end
