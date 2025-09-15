5d05c5722da81a5cd32fb3821e4c1217a31b094d
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-09-15 10:20:40 -0600

    Move QuickScope config to QuickScope

--- lua/configure_theme.lua
+++ lua/configure_theme.lua
@@ -67,10 +67,6 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
     -- Marks
     vim.api.nvim_set_hl(0, "MarkSignHL", { bg = number_background_string })
 
-    -- QuickScope
-    vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#a6f25a", underline = true })
-    vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5af2f2", underline = true })
-
     -- Cmp
     vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
   end,
--- lua/plugins/tools/quick-scope.lua
+++ lua/plugins/tools/quick-scope.lua
@@ -4,5 +4,12 @@ return {
   config = function()
     vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#a6f25a", underline = true })
     vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5af2f2", underline = true })
+
+    vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+      callback = function()
+        vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#a6f25a", underline = true })
+        vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5af2f2", underline = true })
+      end,
+    })
   end,
 }
