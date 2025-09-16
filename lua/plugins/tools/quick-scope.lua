7040c3c395aa780de2baf187d7fd0d9e5e484360
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-09-16 13:19:39 -0600

    Disable quickscope

--- lua/plugins/tools/quick-scope.lua
+++ lua/plugins/tools/quick-scope.lua
@@ -1,6 +1,6 @@
 return {
   "unblevable/quick-scope",
-  cond = true,
+  cond = false,
   config = function()
     vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#a6f25a", underline = true })
     vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5af2f2", underline = true })
