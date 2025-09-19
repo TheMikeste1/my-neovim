8347d67f5c768df8fb603a297c2b2ea36dcb1250
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-09-18 22:02:46 -0600

    Remove unneeded keybinds

--- lua/plugins/tools/comment.lua
+++ lua/plugins/tools/comment.lua
@@ -2,8 +2,4 @@ return {
   "numToStr/Comment.nvim",
   cond = true,
   opts = {},
-  keys = {
-    { "gc", mode = { "v", "n" }, desc = "Comment lines" },
-    { "gb", mode = { "v", "n" }, desc = "Comment block" },
-  },
 }
