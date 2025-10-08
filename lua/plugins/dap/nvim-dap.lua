8acc1a16669655903ce54dfdd139c2a0bb3e68e1
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-10-08 07:55:21 -0600

    Remove keymap files

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -1,9 +1,111 @@
 -- TODO
 return {
   "mfussenegger/nvim-dap",
-  keys = require("keymaps.dap").lazy_keys,
   dependencies = {
     -- "cmakeseer.nvim",
     -- "LiadOz/nvim-dap-repl-highlights", -- TODO: Configure
+  },
+  keys = {
+    {
+      "<F9>",
+      function()
+        require("persistent-breakpoints.api").toggle_breakpoint()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Toggle breakpoint",
+    },
+    {
+      "<F5>",
+      function()
+        require("dap").continue()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Continue debug",
+    },
+    {
+      "<F10>",
+      function()
+        require("dap").step_over()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Step over",
+    },
+    {
+      "<F11>",
+      function()
+        require("dap").step_into()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Step into",
+    },
+    {
+      "<F12>",
+      function()
+        require("dap").step_out()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Step out",
+    },
+    {
+      "<F21>", -- <S-F9>
+      function()
+        -- vim.fn.inputsave()
+        -- local condition = vim.fn.input({
+        -- 	prompt = "Condition: ",
+        -- })
+        -- vim.fn.inputrestore()
+        -- if condition ~= "" then
+        -- 	require("dap").set_breakpoint(condition)
+        -- end
+        require("persistent-breakpoints.api").set_conditional_breakpoint()
+      end,
+      mode = { "v", "n", "i" },
+      desc = "Set conditional breakpoint",
+    },
+    {
+      "<F57>", -- <M-F9>
+      function()
+        vim.fn.inputsave()
+        local log = vim.fn.input({
+          prompt = "Log: ",
+        })
+        vim.fn.inputrestore()
+        if log ~= "" then
+          require("dap").set_breakpoint(nil, nil, log)
+        end
+      end,
+      mode = {
+        "v",
+        "n",
+        "i",
+      },
+      desc = "Set logpoint",
+    },
+    {
+      "<space>dp",
+      function()
+        require("dap.ui.widgets").preview()
+      end,
+      mode = { "n", "v" },
+      desc = "Preview line execution",
+    },
+    {
+      "<space>df",
+      function()
+        local widgets = require("dap.ui.widgets")
+        widgets.centered_float(widgets.frames)
+      end,
+      mode = "n",
+      desc = "View current frame stack",
+    },
+    {
+      "<space>ds",
+      function()
+        local widgets = require("dap.ui.widgets")
+        widgets.centered_float(widgets.scopes)
+      end,
+      mode = "n",
+      desc = "View scopes",
+    },
   },
 }
--- lua/plugins/tools/telescope.lua
+++ lua/plugins/tools/telescope.lua
@@ -1,5 +1,3 @@
-local keymaps = require("keymaps.telescope")
-
 -- Setup the telescope plugin.
 -- @param _ table: The telescope plugin.
 -- @param opts table: The telescope plugin options.
@@ -12,44 +10,55 @@ local function config(_, opts)
   })
 end
 
-local opts = {
-  defaults = {
-    mappings = keymaps.mappings,
-  },
-  pickers = {
-    git_files = {
-      recurse_submodules = true,
-    },
-  },
-  extensions = {
-    cmdline = {
-      picker = {
-        border = true,
-        borderchars = {
-          preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-          results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-        },
-        layout_config = {
-          preview_cutoff = 1,
-        },
-        theme = "dropdown",
-        layout_strategy = "center",
-        results_title = false,
-        sorting_strategy = "ascending",
-        cache_picker = {
-          disabled = true,
-        },
-      },
-    },
-  },
-}
+-- Send the selected entry to the quickfix list and open the list.
+-- @param prompt_buffer_number number: The prompt buffer number.
+local function send_to_quickfix_list(prompt_buffer_number)
+  require("telescope.actions").smart_send_to_qflist(prompt_buffer_number)
+  require("snacks").picker.qflist()
+end
 
 return {
   "nvim-telescope/telescope.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = config,
-  keys = keymaps.lazy_keys,
-  opts = opts,
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<M-C-q>"] = send_to_quickfix_list,
+        },
+        n = {
+          ["<M-C-q>"] = send_to_quickfix_list,
+        },
+      },
+    },
+    pickers = {
+      git_files = {
+        recurse_submodules = true,
+      },
+    },
+    extensions = {
+      cmdline = {
+        picker = {
+          border = true,
+          borderchars = {
+            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+            prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+            results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+          },
+          layout_config = {
+            preview_cutoff = 1,
+          },
+          theme = "dropdown",
+          layout_strategy = "center",
+          results_title = false,
+          sorting_strategy = "ascending",
+          cache_picker = {
+            disabled = true,
+          },
+        },
+      },
+    },
+  },
 }
