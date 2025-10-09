a9da6c28ae117ad63b2808bcec454a6ec1a68a29
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-10-09 11:23:37 -0600

    Lift init out of once_ftplugin

--- plugin/once_ftplugin.lua
+++ plugin/once_ftplugin.lua
@@ -1,1 +1,14 @@
-require("core_ext.once_ftplugin")
+local source_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "core_ext", "once_ftplugin")
+local files = vim.split(vim.fn.glob(string.format("%s/**/*.lua", source_dir)), "\n")
+
+for _, file in ipairs(files) do
+  local filetype = file:sub(#source_dir + 2, #file - 4)
+  vim.api.nvim_create_autocmd("FileType", {
+    desc = string.format("Run the one-time filetype setup for %s files", filetype),
+    pattern = filetype,
+    once = true,
+    callback = function()
+      require("core_ext.once_ftplugin." .. filetype)
+    end,
+  })
+end
