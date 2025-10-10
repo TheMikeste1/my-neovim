f1d48fa6347e42518afb4f607aa47db7a2db667e
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-10-09 20:01:59 -0600

    Fix not using <leader> notation in keymap helpers

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
 vim.g.mapmoveleader = " "
 
 vim.opt.exrc = true
--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -4,14 +4,14 @@ local M = {}
 ---@param keys string The keys to follow the leader.
 ---@return string keys
 function M.leader(keys)
-  return vim.g.mapleader .. keys
+  return "<leader>" .. keys
 end
 
 --- Creates a mapping with the local leader key at the front.
 ---@param keys string The keys to follow the leader.
 ---@return string keys
 function M.local_leader(keys)
-  return vim.g.maplocalleader .. keys
+  return "<localleader>" .. keys
 end
 
 --- Creates a mapping with the movement leader key at the front.
