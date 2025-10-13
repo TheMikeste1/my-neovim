1407f1644b739d04ee50e9478965996589245b49
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-10-13 11:47:43 -0600

    Use which-key modern

--- lua/plugins/tools/which-key.lua
+++ lua/plugins/tools/which-key.lua
@@ -1,12 +1,7 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  config = function(_, opts)
-    require("which-key").setup(opts)
-  end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "modern"
   },
 }
