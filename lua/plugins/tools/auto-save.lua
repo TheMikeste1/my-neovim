20276d7f9f6d3c45b01012c9e3d653af7b8a97cd
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-10-18 18:24:23 -0600

    Prevent autosave from interfereing with Overseer

--- lua/plugins/tools/auto-save.lua
+++ lua/plugins/tools/auto-save.lua
@@ -5,6 +5,20 @@ return {
       message = "", -- Disable the message output
     },
     trigger_events = { "InsertLeave", "TextChanged" },
+    condition = function(buf_num)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+
+      if
+        fn.getbufvar(buf_num, "&modifiable") == 1 and utils.not_in(
+          fn.getbufvar(buf_num, "&filetype"),
+          { "OverseerForm" }
+        )
+      then
+        return true -- met condition(s), can save
+      end
+      return false -- can't save
+    end,
   },
   lazy = false,
 }
