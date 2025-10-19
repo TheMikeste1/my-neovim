9f9c1f99db3b495d1e4c01b5fb92dcdab9e4b051
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-10-18 18:46:22 -0600

    Harper: ignore 'args'

--- lsp/harper_ls.lua
+++ lsp/harper_ls.lua
@@ -2,10 +2,12 @@ return {
   settings = {
     ["harper-ls"] = {
       linters = {
+        -- <https://writewithharper.com/docs/rules>
         SentenceCapitalization = false,
         SpellCheck = false,
         ToDoHyphen = false,
         RepeatedWords = false,
+        ExpandArgument = false,
       },
       isolateEnglish = false,
     },
