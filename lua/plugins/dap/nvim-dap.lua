afec3ca28f2837b8fa6d58cd8bf2c9fac9c42751
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-11-06 09:03:58 -0700

    Setup dap keybinds

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -1,4 +1,70 @@
 local leader = require("keymaps").leader
+local rapid_leader = require("keymaps").rapid_leader
+
+local function get_condition()
+  -- TODO: Use highlighting for current language
+  vim.fn.inputsave()
+  local condition = vim.fn.input({
+    prompt = "Condition: ",
+  })
+  vim.fn.inputrestore()
+  if condition == "" then
+    return nil
+  end
+  return condition
+end
+
+local function get_hit_condition()
+  vim.fn.inputsave()
+  local condition = vim.fn.input({
+    prompt = "Hit condition: ",
+  })
+  vim.fn.inputrestore()
+  if condition == "" then
+    return nil
+  end
+  return condition
+end
+
+local function get_log_message()
+  vim.fn.inputsave()
+  local message = vim.fn.input({
+    prompt = "Log message: ",
+  })
+  vim.fn.inputrestore()
+  if message == "" then
+    return nil
+  end
+  return message
+end
+
+local function set_conditional_breakpoint()
+  local condition = get_condition()
+  if condition ~= nil then
+    require("dap").set_breakpoint(condition)
+  end
+end
+
+local function set_hit_breakpoint()
+  local condition = get_hit_condition()
+  if condition ~= nil then
+    require("dap").set_breakpoint(nil, condition)
+  end
+end
+
+local function set_logpoint()
+  local message = get_log_message()
+  if message ~= nil then
+    require("dap").set_breakpoint(nil, nil, message)
+  end
+end
+
+local function set_advanced_breakpoint()
+  local condition = get_condition()
+  local hit_condition = get_hit_condition()
+  local message = get_log_message()
+  require("dap").set_breakpoint(condition, hit_condition, message)
+end
 
 -- TODO
 return {
@@ -15,106 +81,23 @@ return {
     vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped" })
   end,
   keys = {
-    {
-      leader("db"),
-      function()
-        require("dap").toggle_breakpoint()
-      end,
-      mode = { "v", "n" },
-      desc = "Toggle breakpoint",
-    },
-    {
-      leader("dc"),
-      function()
-        require("dap").continue()
-      end,
-      mode = { "v", "n" },
-      desc = "Continue debug",
-    },
-    {
-      "<F10>",
-      function()
-        require("dap").step_over()
-      end,
-      mode = { "v", "n" },
-      desc = "Step over",
-    },
-    {
-      "<F11>",
-      function()
-        require("dap").step_into()
-      end,
-      mode = { "v", "n" },
-      desc = "Step into",
-    },
-    {
-      "<F12>",
-      function()
-        require("dap").step_out()
-      end,
-      mode = { "v", "n" },
-      desc = "Step out",
-    },
-    {
-      "<F21>", -- <S-F9>
-      function()
-        -- vim.fn.inputsave()
-        -- local condition = vim.fn.input({
-        -- 	prompt = "Condition: ",
-        -- })
-        -- vim.fn.inputrestore()
-        -- if condition ~= "" then
-        -- 	require("dap").set_breakpoint(condition)
-        -- end
-        require("persistent-breakpoints.api").set_conditional_breakpoint()
-      end,
-      mode = { "v", "n" },
-      desc = "Set conditional breakpoint",
-    },
-    {
-      "<F57>", -- <M-F9>
-      function()
-        vim.fn.inputsave()
-        local log = vim.fn.input({
-          prompt = "Log: ",
-        })
-        vim.fn.inputrestore()
-        if log ~= "" then
-          require("dap").set_breakpoint(nil, nil, log)
-        end
-      end,
-      mode = {
-        "v",
-        "n",
-        "i",
-      },
-      desc = "Set logpoint",
-    },
-    {
-      "<space>dp",
-      function()
-        require("dap.ui.widgets").preview()
-      end,
-      mode = { "n", "v" },
-      desc = "Preview line execution",
-    },
-    {
-      "<space>df",
-      function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.frames)
-      end,
-      mode = "n",
-      desc = "View current frame stack",
-    },
-    {
-      "<space>ds",
-      function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.scopes)
-      end,
-      mode = "n",
-      desc = "View scopes",
-    },
+    -- stylua: ignore start
+    { leader("dt"),      desc = "Terminate",                  mode = { "n" }, function() require("dap").terminate() end },
+    { rapid_leader("n"), desc = "Step over",                  mode = { "n" }, function() require("dap").step_over() end },
+    { rapid_leader("s"), desc = "Step into",                  mode = { "n" }, function() require("dap").step_into() end },
+    { rapid_leader("S"), desc = "Step out",                   mode = { "n" }, function() require("dap").step_out() end },
+    { leader("dc"),      desc = "Continue debug execution",   mode = { "n" }, function() require("dap").continue() end },
+    { leader("dC"),      desc = "Continue debug execution",   mode = { "n" }, function() require("dap").run_to_cursor() end },
+    { leader("dp"),      desc = "Pause execution",            mode = { "n" }, function() require("dap").pause() end },
+    { leader("dbb"),     desc = "Toggle breakpoint",          mode = { "n" }, function() require("dap").toggle_breakpoint() end },
+    { leader("dbc"),     desc = "Set conditional breakpoint", mode = { "n" }, set_conditional_breakpoint },
+    { leader("dbh"),     desc = "Set hit breakpoint",         mode = { "n" }, set_hit_breakpoint },
+    { leader("dbl"),     desc = "Set logpoint",               mode = { "n" }, set_logpoint },
+    { leader("dba"),     desc = "Set advanced breakpoint",    mode = { "n" }, set_advanced_breakpoint },
+    { leader("dbe"),     desc = "Set exception breakpoint",   mode = { "n" }, function() require("dap").set_exception_breakpoints() end },
+    { leader("dlp"),     desc = "Preview line execution",     mode = { "n" }, function() require("dap.ui.widgets").preview() end },
+    { leader("df"),      desc = "View current frame stack",   mode = { "n" }, function() require("dap.ui.widgets").centered_float(require("dap.ui.widgets").frames) end },
+    { leader("ds"),      desc = "View scopes",                mode = { "n" }, function() require("dap.ui.widgets").centered_float(require("dap.ui.widgets").scopes) end },
+    -- stylua: ignore end
   },
 }
--- lua/plugins/ui/nvim-dap-ui.lua
+++ lua/plugins/ui/nvim-dap-ui.lua
@@ -1,22 +1,13 @@
 return {
   "rcarriga/nvim-dap-ui",
-
   config = function()
     local dap, dapui = require("dap"), require("dapui")
     dapui.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    dap.listeners.before.attach.dapui_config = dapui.open
+    dap.listeners.before.launch.dapui_config = dapui.open
+    dap.listeners.before.event_terminated.dapui_config = dapui.close
+    dap.listeners.before.event_exited.dapui_config = dapui.close
   end,
   dependencies = {
     "mfussenegger/nvim-dap",
