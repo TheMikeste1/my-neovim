518913cd82cee5d505c96054a6da346c8361bb54
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2025-11-10 10:43:47 -0700

    Add LLDB configs to DAP

--- lua/configs/dap/c.lua
+++ lua/configs/dap/c.lua
@@ -1,6 +1,23 @@
 ---@return string path The path to the executable to run.
 local function get_executable_path()
-  return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+  local cwd = vim.fn.getcwd()
+  local candidates = vim.fn.glob(cwd .. "/build/**/*", false, true)
+  local executable_candidates = {}
+  for _, candidate in ipairs(candidates) do
+    local file_type = vim.fn.getftype(candidate)
+    if file_type ~= "file" then
+      goto continue
+    end
+
+    local perms = vim.fn.getfperm(candidate)
+    if string.match(perms, "x") then
+      table.insert(executable_candidates, candidate)
+    end
+
+    ::continue::
+  end
+
+  return vim.fn.input("Executable: ", executable_candidates[1] or (cwd .. "/"), "file")
 end
 
 ---@param target cmakeseer.cmake.api.codemodel.Target The CMakeSeer target.
@@ -59,30 +76,63 @@ function M.adapters()
         "mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7"
       ),
     },
+    lldb = {
+      type = "executable",
+      command = function()
+        local maybe_path = vim.fn.exepath("lldb-dap")
+        return maybe_path ~= nil and maybe_path or "lldb-dap"
+      end,
+      name = "lldb",
+    },
   }
 end
 
 ---@return dap.Configuration
 function M.configurations()
-  local configs = generate_cmakeseer_configurations()
-  table.insert(configs, {
-    name = "Launch executable",
-    type = "cppdbg",
-    request = "launch",
-    MIMode = "gdb",
-    miDebuggerPath = "gdb",
-    program = get_executable_path,
-    cwd = "${workspaceFolder}",
-    stopAtEntry = false,
-    setupCommands = {
-      {
-        text = "-enable-pretty-printing",
-        description = "enable pretty printing",
-        ignoreFailures = false,
+  local configs = {
+    {
+      name = "CPPDBG: Launch executable",
+      type = "cppdbg",
+      request = "launch",
+      MIMode = "gdb",
+      miDebuggerPath = "gdb",
+      program = get_executable_path,
+      cwd = "${workspaceFolder}",
+      stopAtEntry = false,
+      setupCommands = {
+        {
+          text = "-enable-pretty-printing",
+          description = "enable pretty printing",
+          ignoreFailures = false,
+        },
       },
     },
-  })
+    {
+      name = "LLDB: Launch executable",
+      type = "lldb",
+      request = "launch",
+      program = get_executable_path,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+      runInTerminal = false,
+      console = "integratedTerminal",
+    },
+    {
+      name = "LLDB: Attach to process",
+      type = "lldb",
+      request = "attach",
+      pid = require("dap.utils").pick_process,
+      args = {},
+    },
+  }
 
+  local cmakeseer_configs = generate_cmakeseer_configurations()
+  configs = vim.list_extend(configs, cmakeseer_configs)
+
+  -- table.sort(configs, function(a, b)
+  --   return a.name < b.name
+  -- end)
   return configs
 end
 
