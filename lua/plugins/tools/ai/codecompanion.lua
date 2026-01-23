44c7f6a0842fd23b37ae1bfc8a41577a45da4114
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-01-23 14:48:14 -0700

    Speed up launch

--- lua/plugins/tools/ai/codecompanion.lua
+++ lua/plugins/tools/ai/codecompanion.lua
@@ -1,6 +1,7 @@
 ---@module "codecompanion"
 return {
   "olimorris/codecompanion.nvim",
+  event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
