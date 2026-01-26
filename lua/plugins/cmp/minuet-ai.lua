e93dc0bccee886402069f4db31bf874bce05a1fd
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-01-26 08:04:39 -0700

    Update minuet

--- lua/plugins/cmp/minuet-ai.lua
+++ lua/plugins/cmp/minuet-ai.lua
@@ -1,25 +1,35 @@
 return {
   "milanglacier/minuet-ai.nvim",
   dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    require("minuet").setup({
-      provider = "openai_fim_compatible",
-      n_completions = 1,
-      context_window = 512,
-      provider_options = {
-        openai_fim_compatible = {
-          -- For Windows users, TERM may not be present in environment variables.
-          -- Consider using APPDATA instead.
-          api_key = "TERM",
-          name = "Ollama",
-          end_point = "http://localhost:11434/v1/completions",
-          model = "qwen2.5-coder:7b",
-          optional = {
-            max_tokens = 56,
-            top_p = 0.9,
-          },
+  opts = {
+    provider = "openai_fim_compatible",
+    n_completions = 1,
+    context_window = 512,
+    provider_options = {
+      openai_fim_compatible = {
+        -- For Windows users, TERM may not be present in environment variables.
+        -- Consider using APPDATA instead.
+        api_key = "TERM",
+        name = "Ollama",
+        end_point = "http://localhost:11434/v1/completions",
+        model = "qwen2.5-coder:7b",
+        optional = {
+          max_tokens = 56,
+          top_p = 0.9,
         },
       },
-    })
-  end,
+    },
+    virtualtext = {
+      auto_trigger_ft = { "*" },
+      show_on_completion_menu = false,
+      keymap = {
+        accept = "<C-g><CR>",
+        accept_line = nil,
+        accept_n_lines = nil,
+        next = "<C-g>n",
+        prev = "<C-g>p",
+        dismiss = nil,
+      },
+    },
+  },
 }
