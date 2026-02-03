9b3166e0e86871c107a00c01243af7fc8daf11ee
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-02-03 14:08:45 -0700

    clangd improvements

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,4 +1,4 @@
-local nproc = #vim.loop.cpu_info() / 2
+local nproc = #vim.loop.cpu_info()
 if nproc == 0 then
   nproc = 8
 end
@@ -13,5 +13,19 @@ return {
     "--malloc-trim",
     "--all-scopes-completion",
     string.format("-j=%d", nproc),
+
+    -- Background indexing
+    "--background-index",
+
+    -- Completion improvements
+    "--completion-style=detailed", -- More detailed completion items
+    "--function-arg-placeholders=true", -- Adds placeholders for function args
+
+    -- Better cross-reference info
+    "--limit-references=0", -- Don't limit find-references (default is 1000)
+    "--limit-results=0", -- Don't limit other results
+
+    -- Preamble/PCH optimization
+    "--pch-storage=memory", -- Keep precompiled headers in memory (faster)
   },
 }
