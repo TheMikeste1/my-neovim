3f02200e133e8b83cccc6bbc561f31190c4a9478
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-03-31 09:31:33 -0600

    Add COSMOS tree-sitter

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -1,6 +1,15 @@
 ---@class opts TSConfig
 function config(_, opts)
   require("nvim-treesitter.configs").setup(opts)
+  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  parser_config.cosmos = {
+    install_info = {
+      url = "https://github.com/TheMikeste1/tree-sitter-cosmos.git",
+      files = { "src/parser.c" },
+      queries = "queries/neovim", -- also install queries from given directory
+    },
+    filetype = "cosmos",
+  }
 end
 
 return {
