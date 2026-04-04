61472003e89a705da4f6f40f4c701ff94ecaba5f
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-04-03 21:24:08 -0600

    Update treesitter
    It's archived, so now what do I do?

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -1,20 +1,14 @@
 ---@class opts TSConfig
 function config(_, opts)
-  require("nvim-treesitter.configs").setup(opts)
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.cosmos = {
-    install_info = {
-      url = "https://github.com/TheMikeste1/tree-sitter-cosmos.git",
-      files = { "src/parser.c" },
-      queries = "queries/neovim", -- also install queries from given directory
-    },
-    filetype = "cosmos",
-  }
+  require("nvim-treesitter").setup(opts)
+
+  vim.treesitter.language.register("cosmos", { "cosmos" })
 end
 
 return {
   "nvim-treesitter/nvim-treesitter",
   cond = true,
+  branch = "main",
   build = ":TSUpdate",
   cmd = { "TSUpdateSync" },
   dependencies = {
