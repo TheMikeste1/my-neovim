3611d6cfaad5131f6aa112c5f8b26083a322d92c
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-04-08 11:55:09 -0600

    Add xTEDS icon

--- lua/plugins/ui/nvim-web-devicons.lua
+++ lua/plugins/ui/nvim-web-devicons.lua
@@ -1,4 +1,12 @@
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true,
+  opts = {
+    override_by_extension = {
+      ["xteds"] = {
+        icon = "󰗀",
+        color = "#16c44a"
+      },
+    },
+  },
 }
