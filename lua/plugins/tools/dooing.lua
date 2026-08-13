3efab0254aa3c8f78f0b9a8183e9c66ba98c63f5
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-08-13 11:48:54 -0600

    Update dooing

--- lua/plugins/tools/dooing.lua
+++ lua/plugins/tools/dooing.lua
@@ -3,9 +3,14 @@ local leader = require("keymaps").leader
 return {
   "atiladefreitas/dooing",
   opts = {
+    ui = { style = "modern" },
     window = {
-      width = 55 * 2,
-      height = 20 * 2,
+      dimensions = function()
+        return {
+          width = math.max(40, math.floor(vim.o.columns * 0.4)),
+          height = math.max(10, math.floor(vim.o.lines * 0.6)),
+        }
+      end,
     },
     per_project = {
       on_missing = "auto_create",
