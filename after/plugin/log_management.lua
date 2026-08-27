4706ee98a276866ad5a1a6b6fa7750227fb0d114
Michael Hegerhorst <michael.hegerhorst+work@gmail.com>
2026-08-27 14:27:39 -0600

    Add OpenLog

--- after/plugin/log_management.lua
+++ after/plugin/log_management.lua
@@ -33,7 +33,6 @@ vim.api.nvim_create_user_command("ClearLog", function(opts)
     else
       vim.notify("Provide a log to clear. Options: " .. logs_string, vim.log.levels.ERROR)
     end
-
     return
   end
 
@@ -75,4 +74,73 @@ end, {
   end,
 })
 
--- TODO: Open log command like ConformLog
+vim.api.nvim_create_user_command("OpenLog", function(opts)
+  local args = vim.split(opts.args, " ")
+  if #args > 2 then
+    vim.notify("OpenLog takes one to two parameters:\n\t:OpenLog <log_name> [mode]", vim.log.levels.ERROR)
+    return
+  end
+
+  local log_name = args[1]
+  local path = vim.fs.joinpath(vim.fn.stdpath("log"), log_name) .. ".log"
+  if not vim.uv.fs_stat(path) then
+    local logs_string = get_log_names_as_string()
+    if logs_string == nil then
+      vim.notify("No logs available!", vim.log.levels.WARN)
+    else
+      vim.notify("Invalid log `" .. log_name .. "`. Options: " .. logs_string, vim.log.levels.ERROR)
+    end
+    return
+  end
+
+  local mode = args[2] ~= "" and args[2] or "float"
+  if mode == "float" then
+    -- Create a scratch buffer specifically for the floating window
+    local buf = vim.api.nvim_create_buf(false, true)
+    local width = vim.o.columns - 3
+    local height = vim.o.lines - 2
+    vim.api.nvim_open_win(buf, true, {
+      relative = "editor",
+      width = width,
+      height = height,
+      col = math.floor((vim.o.columns - width)),
+      row = math.floor((vim.o.lines - height)),
+      style = "minimal",
+      border = "rounded",
+    })
+    vim.cmd("edit " .. vim.fn.fnameescape(path))
+  elseif mode == "split" then
+    vim.cmd("split " .. vim.fn.fnameescape(path))
+  elseif mode == "vsplit" then
+    vim.cmd("vsplit " .. vim.fn.fnameescape(path))
+  elseif mode == "current" then
+    vim.cmd("edit " .. vim.fn.fnameescape(path))
+  else
+    vim.notify("Invalid argument for OpenLog. Modes: float, split, vsplit, or current", vim.log.levels.ERROR)
+    return
+  end
+end, {
+  nargs = "+",
+  desc = "Opens a log file",
+  complete = function(ArgLead, CmdLine)
+    local command_parts = vim.split(CmdLine, " ")
+    local possibilities = {}
+    if #command_parts < 3 then
+      possibilities = get_log_names()
+    elseif #command_parts == 3 then
+      possibilities = { "float", "split", "vsplit", "current" }
+    end
+
+    local matches = {}
+    local nonmatches = {}
+    for _, possiblity in ipairs(possibilities) do
+      if vim.startswith(possiblity, ArgLead) then
+        table.insert(matches, possiblity)
+      else
+        table.insert(nonmatches, possiblity)
+      end
+    end
+
+    return vim.list_extend(matches, nonmatches)
+  end,
+})
